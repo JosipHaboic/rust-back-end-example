1c0c3f6cce402cbff8f0fd83d20252cf1726852e
jhaboic <josiphaboic@gmail.com>
2019-11-26 21:12:49 +0100

    update: create use mod and move code under it

--- src/transaction_scripts/user.rs
+++ src/transaction_scripts/user.rs
@@ -1,54 +1,58 @@
 #![allow(dead_code)]
-use crate::core::traits::domain_logic::TransactionScript;
-use crate::core::types::sqlite3::{Params};
-use crate::models::{User};
-use rusqlite::{Connection};
 
+pub mod user {
+	use crate::core::traits::domain_logic::TransactionScript;
+	use crate::core::types::sqlite3::{Params};
+	use crate::models::{User};
+	use rusqlite::{Connection};
 
-pub struct FindUserByUsername<'a> {
-	pub username: &'a str,
-}
-
-impl <'a>FindUserByUsername<'a> {
-	pub fn new(username: &'a str) -> FindUserByUsername {
-		FindUserByUsername { username }
+	pub struct FindByUsername<'a> {
+		pub username: &'a str,
 	}
-}
 
-impl <'a>TransactionScript for FindUserByUsername<'a> {
-	type Output = Option<User>;
-	type Connection = Connection;
-	type Params = Params;
+	impl <'a>FindByUsername<'a> {
+		pub fn new(username: &'a str) -> FindByUsername {
+			FindByUsername { username }
+		}
+	}
 
-	fn execute(
-		self: &Self,
-		connection: &Self::Connection,
-		params: &Self::Params)
-	-> Self::Output {
-		let mut statement = connection.prepare("
-			SELECT * FROM users WHERE username = ?1
-		").unwrap();
+	impl <'a>TransactionScript for FindByUsername<'a> {
+		type Output = Option<User>;
+		type Connection = Connection;
+		type Params = Params;
 
-		match statement.query_row(&[params.get("username")], |row| {
-			Ok(
-				User {
-					uuid: row.get(0).unwrap(),
-					username: row.get(1).unwrap(),
-					password: row.get(2).unwrap(),
-					inserted_at: row.get(3).unwrap()
-				}
-			)
-		}) {
-			Ok(result) => Some(result),
-			Err(_) => None
+		fn execute(
+			self: &Self,
+			connection: &Self::Connection,
+			params: &Self::Params)
+		-> Self::Output {
+			let mut statement = connection.prepare("
+				SELECT * FROM users WHERE username = ?1
+			").unwrap();
+
+			match statement.query_row(&[params.get("username")], |row| {
+				Ok(
+					User {
+						uuid: row.get(0).unwrap(),
+						username: row.get(1).unwrap(),
+						password: row.get(2).unwrap(),
+						inserted_at: row.get(3).unwrap()
+					}
+				)
+			}) {
+				Ok(result) => Some(result),
+				Err(_) => None
+			}
 		}
 	}
 }
 
-
 #[test]
 fn test_find_user_by_username() {
-	use crate::core::types::sqlite3::{Value};
+	use crate::core::types::sqlite3::{Value, Params};
+	use crate::core::traits::domain_logic::TransactionScript;
+	use user::{FindByUsername};
+	use rusqlite::{Connection};
 
 	let connection = Connection::open_in_memory().unwrap();
 
@@ -64,7 +68,7 @@ fn test_find_user_by_username() {
 		VALUES ('98ds-9s8ds9d-98ds-9d98s', 'Josip', '0d9s0d909ds');
 	").unwrap();
 
-	let user_find_by_username = FindUserByUsername::new("Josip");
+	let user_find_by_username = FindByUsername::new("Josip");
 
 	let mut search_params = Params::new();
 	search_params.insert(
